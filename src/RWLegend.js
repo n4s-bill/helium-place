3f1b443aa4b345ae957e5c1d0d1c929fda415996
Bill Johnson <bill.johnson@scribendi.com>
2021-05-19 19:14:16 -0400

    Test commit

--- src/RWLegend.js
+++ src/RWLegend.js
@@ -6,22 +6,22 @@ export default class RWLegend extends React.Component {
       <div className="rwlegend">
         <h4>Reward Scale</h4>
         <div className="key">
-          <span style={{ background: "#1C1AAF" }}></span> 1.0
+          <span style={{ background: "#1C1AAF" }}></span>&nbsp; 1.0
         </div>
         <div className="key">
-          <span style={{ background: "#0066E7" }}></span> 0.9
+          <span style={{ background: "#0066E7" }}></span>&nbsp; 0.9
         </div>
         <div className="key">
-          <span style={{ background: "#00FFFF" }}></span> 0.8
+          <span style={{ background: "#00FFFF" }}></span>&nbsp; 0.8
         </div>
         <div className="key">
-          <span style={{ background: '#FFDA13' }}></span> 0.5
+          <span style={{ background: '#FFDA13' }}></span>&nbsp; 0.5
         </div>
         <div className="key">
-          <span style={{ background: '#F18009' }}></span> 0.3
+          <span style={{ background: '#F18009' }}></span>&nbsp; 0.3
         </div>
         <div className="key">
-          <span style={{ background: '#D4070F' }}></span> 0.1
+          <span style={{ background: '#D4070F' }}></span>&nbsp; 0.1
         </div>
       </div>
     )
